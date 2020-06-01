5bf1ea4d835aa77abd8450f8167c71fcfffa52eb
kotunovda <66233388+kotunovda@users.noreply.github.com>
2020-06-01 06:19:18 +0300

    Create Types.hpp

---
+++ Types.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Tokens {   
+  enum Types {VAR, COS, SIN, TG, CTG, EXP, LN, NUM, 
+	      PLUS, MINUS, MULT, DIV, UPPER, LBR, RBR, 
+	      FUNC, OP};
+}
+
+namespace Syntax {
+  enum Types {UNO, BIN, NUM, VAR};
+}
